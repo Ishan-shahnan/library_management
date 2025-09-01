6879956a27a9dc5dd97ed3ffdeb64686cdbc5da4
Ishan Shahnan <162598177+Ishan-shahnan@users.noreply.github.com>
2025-09-01 19:54:48 +0600

    solve2

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Add the project directory to Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
